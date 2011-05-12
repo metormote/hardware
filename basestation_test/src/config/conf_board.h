406e540bec3e5a75cb4ebc9e62090642505ea70b
Erik Rosen <erik.rosen@metormote.com>
2011-05-12 21:21:41 +0200

    Added AVR Studio project file

--- basestation_test/src/config/conf_board.h
+++ basestation_test/src/config/conf_board.h
@@ -8,6 +8,13 @@
 #ifndef CONF_BOARD_H
 #define CONF_BOARD_H
 
+/* The difference, measured in seconds, between the indicated 
+ * time and midnight, January 1, 1970 UTC.
+ * For easy compatibility with Java-time standard.
+ * 5 May 2011 1800 EST
+ */
+#define TIME_ZERO 1304632800
+
 // Enable AT45DBX Component.
 #define CONF_BOARD_AT45DBX
 
--- basestation_test/src/main.c
+++ basestation_test/src/main.c
@@ -1,7 +1,7 @@
 /**
  * \file
  *
- * \brief Empty user application template
+ * \brief Basestation Test
  *
  */
 
@@ -13,13 +13,73 @@
 #include "at45dbx.h"
 #include "conf_board.h"
 
+
+/* Constants */
+
+#define ANT_USART                        &USARTD0
+
+#define GPRS_USART                       &USARTC0
+
+
+// ANT USART options.
+static usart_rs232_options_t ANT_USART_OPTIONS = {
+	.baudrate = 57600,
+	.charlength = USART_CHSIZE_8BIT_gc,
+	.paritytype = USART_PMODE_DISABLED_gc,
+	.stopbits = false
+};
+
+// GPRS USART options.
+static usart_rs232_options_t GPRS_USART_OPTIONS = {
+	.baudrate = 38400,
+	.charlength = USART_CHSIZE_8BIT_gc,
+	.paritytype = USART_PMODE_DISABLED_gc,
+	.stopbits = false
+};
+
+static void init(void) {
+	//initialize interrupt controller
+	pmic_init();
+	//initialize board
+	board_init();
+	//initialize system clock
+	sysclk_init();
+	//initialize sleep manager
+	sleepmgr_init();
+	//initialize data flash
+	at45dbx_init();
+	//initialize real time counter
+	rtc_init();
+	
+	rtc_set_time(TIME_ZERO);
+	
+	
+	// Initialize ant usart driver in RS232 mode
+	usart_init_rs232(ANT_USART, &ANT_USART_OPTIONS);
+	usart_set_rx_interrupt_level(ANT_USART, PMIC_LVL_HIGH);
+	usart_set_tx_interrupt_level(ANT_USART, PMIC_LVL_HIGH);
+	usart_set_dre_interrupt_level(ANT_USART, PMIC_LVL_HIGH);
+	
+	// Initialize gprs usart driver in RS232 mode
+	usart_init_rs232(GPRS_USART, &GPRS_USART_OPTIONS);
+	usart_set_rx_interrupt_level(GPRS_USART, PMIC_LVL_HIGH);
+	usart_set_tx_interrupt_level(GPRS_USART, PMIC_LVL_HIGH);
+	usart_set_dre_interrupt_level(GPRS_USART, PMIC_LVL_HIGH);
+	
+	
+}
+
 int main (void)
 {
-	sysclk_init();
+  
+	//initialize all modules
+	init();
 	
-	board_init();
+	while (1)
+      ;
+
+  /* There was an error starting the OS if we reach here */
+  return (0);
 	
-	at45dbx_init();
-	
-	// Insert application code here, after the board has been initialized.
 }
+
