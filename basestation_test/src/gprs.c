ed1b73de76f885dfb0f9941b6b821b58696acad2
Niklas Tinglöf <niklo@dynalogic.se>
2011-05-31 22:10:16 +0200

    added init and control routines for reset, on/off

--- basestation_test/src/gprs.c
+++ basestation_test/src/gprs.c
@@ -6,7 +6,17 @@
  */ 
 
 #include <asf.h>
+#include <util/delay.h>
 #include "gprs.h"
+
+
+int8_t gprs_init(void)
+{
+	gpio_set_pin_low(GPRS_ON_OFF);
+	gpio_set_pin_low(GPRS_RESET);
+	
+	return STATUS_OK;
+}
 
 enum status_code gprs_send(uint8_t* data, uint8_t size)
 {
@@ -20,7 +30,6 @@ enum status_code gprs_send(uint8_t* data, uint8_t size)
 		// wait for empty data register...
 		while (usart_data_register_is_empty(usart) == false)
 		{
-			
 		}
 	
 		
@@ -48,6 +57,34 @@ enum status_code gprs_receive(uint8_t* data, uint8_t size)
 		*p = (usart)->DATA;
 		p++;
 	}
+	
+	return STATUS_OK;
+}
+
+
+int8_t gprs_reset(uint8_t state)
+{
+	// TODO: implement...
+	gpio_set_pin_high(GPRS_RESET);
+	_delay_ms(200); // NOTE_ F_CLK needs to be set properly!!!
+	gpio_set_pin_low(GPRS_RESET);
+	return STATUS_OK;
+}
+
+int8_t gprs_on(void)
+{
+	gpio_set_pin_high(GPRS_ON_OFF);
+	_delay_ms(5000); // NOTE_ F_CLK needs to be set properly!!!
+	gpio_set_pin_low(GPRS_ON_OFF);
+	
+	return STATUS_OK;
+}
+
+int8_t gprs_off(void)
+{
+	gpio_set_pin_high(GPRS_ON_OFF);
+	_delay_ms(4000); // NOTE_ F_CLK needs to be set properly!!!
+	gpio_set_pin_low(GPRS_ON_OFF);
 	
 	return STATUS_OK;
 }
--- basestation_test/src/gprs.h
+++ basestation_test/src/gprs.h
@@ -9,7 +9,12 @@
 #ifndef GPRS_H_
 #define GPRS_H_
 
+int8_t gprs_init(void);
+
 enum status_code gprs_send(uint8_t* data, uint8_t size);
 enum status_code gprs_receive(uint8_t* data, uint8_t size);
+
+int8_t gprs_reset(uint8_t state);
+int8_t gprs_on(void);
 
 #endif /* GPRS_H_ */
